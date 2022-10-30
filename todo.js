53e932fcf9c268037bab41de4997196e5a598aa9
hima <himaanshuvasantham3@gmail.com>
2022-10-30 17:26:56 +0530

    init

---
+++ todo.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-undef */
+const todoList = () => {
+  all = [];
+
+  function add(todoItem) {
+    all.push(todoItem);
+  }
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+  const ndate = () => new Date();
+  const overdue = () => {
+    // Write the date check condition here and return the array of overdue items accordingly.
+    // FILL YOUR CODE HERE
+    return all.filter(
+      (dolist) => dolist.dueDate < ndate().toLocaleDateString("en-CA")
+    );
+  };
+
+  const dueToday = () => {
+    // Write the date check condition here and return the array of todo items that are due today accordingly.
+    // FILL YOUR CODE HERE
+    return all.filter(
+      (dolist) => ndate().toLocaleDateString("en-CA") == dolist.dueDate
+    );
+  };
+
+  const dueLater = () => {
+    // Write the date check condition here and return the array of todo items that are due later accordingly.
+    // FILL YOUR CODE HERE
+    return all.filter(
+      (dolist) => dolist.dueDate > ndate().toLocaleDateString("en-CA")
+    );
+  };
+
+  const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string as per the format given above.
+    // FILL YOUR CODE HERE
+    return list
+      .map((todos) => {
+        const complete = todos.completed ? "x" : " ";
+        return `[${complete}] ${todos.title} ${
+          todos.dueDate == today ? " " : todos.dueDate
+        }`;
+      })
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+module.exports = todoList;
